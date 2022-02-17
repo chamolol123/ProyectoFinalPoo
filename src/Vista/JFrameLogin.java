1ac36ec6267aa7b4682c03da181c2cd2685d6aab
ParraJesus <jgparra@unicauca.edu.co>
2022-02-17 14:49:27 -0500

    Lo arreglé!

--- src/Vista/JFrameLogin.java
+++ src/Vista/JFrameLogin.java
@@ -253,7 +253,7 @@ public class JFrameLogin extends javax.swing.JFrame {
              this.setVisible(false);
              
          }else{
-             frmEstudiante estudiante = new frmEstudiante();
+             JFrameEstudiante estudiante = new JFrameEstudiante();
              estudiante.setVisible(true);
              this.setVisible(false);
          }
