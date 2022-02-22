8c7325fb154c1ba44248c346b0eec3854a87a0d5
ParraJesus <jgparra@unicauca.edu.co>
2022-02-22 17:11:58 -0500

    x

---
+++ src/Modelo/Tablas.java
@@ -0,0 +1,12 @@
+package Modelo;
+
+import javax.swing.JOptionPane;
+
+public class Tablas {
+    
+    int filas = 3, colum = 4;
+    
+    float object [][] = new float[filas][colum];
+             
+    
+}
